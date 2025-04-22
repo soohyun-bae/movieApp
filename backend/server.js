ca81e840e8ffc438cfda2b1e22ba8714c1e699d5
soohyun <qosuh58@gmail.com>
2025-04-22 18:45:41 +0900

    chore:move code

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,7 @@ import cookieParser from 'cookie-parser';
 dotenv.config();
 
 const app = express();
+app.use(express.json());
 
 const allowedOrigins = [
   'https://frontend-one-pi-24.vercel.app',
@@ -17,7 +18,6 @@ app.use(cors({
   origin: allowedOrigins,
   credentials: true,
 }));
-app.use(express.json());
 app.use(cookieParser());
 
 app.use('/movie', routes);
