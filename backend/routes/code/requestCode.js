932ff6e7ab0ee40b5c8ce1467e1378ef4ee635aa
soohyun <qosuh58@gmail.com>
2025-04-23 13:15:31 +0900

    feat: edit requestCode code

--- backend/routes/code/requestCode.js
+++ backend/routes/code/requestCode.js
@@ -14,24 +14,31 @@ const transporter = nodemailer.createTransport({
 });
 
 router.post('/', async (req, res) => {
-  const { email } = req.body;
+  console.log('api debuging: request-code');
 
-  const code = crypto.randomBytes(3).toString('hex');
-  const expiresAt = new Date(Date.now() + 1000 * 60 * 5);
+  try {
+    const { email } = req.body;
 
-  await sql`
+    const code = crypto.randomBytes(3).toString('hex');
+    const expiresAt = new Date(Date.now() + 1000 * 60 * 5);
+
+    await sql`
   INSERT INTO email_verifications (email, code, expires_at)
   VALUES(${email}, ${code}, ${expiresAt})
   `;
 
-  await transporter.sendMail({
-    from: process.env.EMAIL_USER,
-    to: email,
-    subject: '이메일 인증코드',
-    text: `인증코드는: ${code}`,
-  });
+    await transporter.sendMail({
+      from: process.env.EMAIL_USER,
+      to: email,
+      subject: '이메일 인증코드',
+      text: `인증코드는: ${code}`,
+    });
 
-  res.json({ message: '인증코드 전송 완료' });
+    res.json({ message: '인증코드 전송 완료' });
+  } catch (error) {
+    console.error('request-code error: ', error);
+    res.status(500).json({ error: 'server error' });
+  }
 });
 
 export default router;
