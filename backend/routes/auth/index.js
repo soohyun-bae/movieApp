a24f244078d8fb31f88094ee282aaf3c07a4c14c
soohyun <qosuh58@gmail.com>
2025-04-29 18:39:28 +0900

    feat: add meRouter

--- backend/routes/auth/index.js
+++ backend/routes/auth/index.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import loginRouter from './login.js';
-// import meRouter from './me.js';
+import meRouter from './me.js';
 import refreshRouter from './refresh.js';
 import registerRouter from './register.js';
 
@@ -8,7 +8,7 @@ const router = express.Router();
 
 router.use('/register', registerRouter);
 router.use('/login', loginRouter);
-// router.use('/me', meRouter);
+router.use('/me', meRouter);
 router.use('/refresh', refreshRouter);
 
 export default router;
