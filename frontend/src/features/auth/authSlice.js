95205a4e6ff5656062051de642c1a08fc2d4b014
soohyun <qosuh58@gmail.com>
2025-04-29 18:31:18 +0900

    feat: edit authSlice initialState

--- frontend/src/features/auth/authSlice.js
+++ frontend/src/features/auth/authSlice.js
@@ -3,7 +3,7 @@ import { createSlice } from "@reduxjs/toolkit";
 export const authSlice = createSlice({
   name: "auth",
   initialState: {
-    user
+    user: {},
   },
   reducers: {
     setUser(state, action) {
